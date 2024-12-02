cadbbfc2f623411af63a6dbf47f0f6fda2bcc13a
Tommy Crews <twcrews96@gmail.com>
2024-12-01 20:44:27 -0600

    Add XML doc fixes to generator extensions.

--- Crews.PlanningCenter.Models.Generators/Extensions/StringExtensions.cs
+++ Crews.PlanningCenter.Models.Generators/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Crews.PlanningCenter.Models.Generators.Extensions;
 
 public static class StringExtensions
@@ -7,6 +9,11 @@ public static class StringExtensions
 		string indent = indentSpaces > 0 ? new(' ', indentSpaces) : string.Empty;
 
 		string content = string.Join('\n', target
+			.Trim()
+			.RecursivelyRemoveTripleNewLines()
+			.FixAmpersands()
+			.FixLinks()
+			.FixInlineCode()
 			.Split('\n', StringSplitOptions.TrimEntries)
 			.Select(substring => $"{indent}/// {substring}"));
 
@@ -16,4 +23,19 @@ public static class StringExtensions
 	}
 
 	public static string ToSnakeCase(this string target) => target.Replace('-', '_');
+
+	private static string RecursivelyRemoveTripleNewLines(this string target)
+	{
+		if (target.Contains("\n\n\n"))
+		{
+			return target.Replace("\n\n\n", "\n\n").RecursivelyRemoveTripleNewLines();
+		}
+		return target;
+	}
+
+	private static string FixAmpersands(this string target) => Regex.Replace(target, @"&(?!amp;)", "&amp;");
+
+	private static string FixLinks(this string target) => Regex.Replace(target, @"\[(.*?)\]\((.*?)\)", "<a href=\"$2\">$1</a>");
+
+	private static string FixInlineCode(this string target) => Regex.Replace(target, @"`([^`]+)`", "<c>$1</c>");
 }
