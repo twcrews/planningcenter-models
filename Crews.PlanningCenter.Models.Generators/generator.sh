da9b15871ac4631914e6cb691633c0f88c265021
Tommy Crews <twcrews96@gmail.com>
2025-09-22 13:11:15 -0500

    Update generator dependencies and script.

--- Crews.PlanningCenter.Models.Generators/generator.sh
+++ Crews.PlanningCenter.Models.Generators/generator.sh
@@ -2,27 +2,29 @@
 SECONDS=0
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-HTTP_EXTENSIONS_VERSION="2.0.0"
-HTTP_EXTENSIONS_FRAMEWORK="net8.0"
+HTTP_EXTENSIONS_VERSION="3.0.0"
+HTTP_EXTENSIONS_FRAMEWORK="netstandard2.0"
 
-PRIMITIVES_EXTENSIONS_VERSION="1.1.2"
-PRIMITIVES_EXTENSIONS_FRAMEWORK="net8.0"
+PRIMITIVES_EXTENSIONS_VERSION="1.2.1"
+PRIMITIVES_EXTENSIONS_FRAMEWORK="netstandard2.0"
 
 MS_OPTIONS_VERSION="9.0.0"
 MS_OPTIONS_FRAMEWORK="net9.0"
+
+GENERATOR_FRAMEWORK="net9.0"
 
 printf "Building project...\n"
 dotnet build "$SCRIPT_DIR"
 printf "\n\n================\n\n"
 
-printf "You can safely ignore errors that begin with 'Could not write output file'."
+printf "You can safely ignore errors that begin with 'Could not write output file'.\n\n"
 
 printf "Generating model entities...\n"
 t4 \
 	-r "$HOME/.nuget/packages/crews.extensions.http/$HTTP_EXTENSIONS_VERSION/lib/$HTTP_EXTENSIONS_FRAMEWORK/Crews.Extensions.Http.dll" \
 	-r "$HOME/.nuget/packages/crews.extensions.primitives/$PRIMITIVES_EXTENSIONS_VERSION/lib/$PRIMITIVES_EXTENSIONS_FRAMEWORK/Crews.Extensions.Primitives.dll" \
 	-r "$HOME/.nuget/packages/microsoft.extensions.options/$MS_OPTIONS_VERSION/lib/$MS_OPTIONS_FRAMEWORK/Microsoft.Extensions.Options.dll" \
-	-r "$SCRIPT_DIR/../Crews.PlanningCenter.Models.Generators/bin/Debug/net8.0/Crews.PlanningCenter.Models.Generators.dll" \
+	-r "$SCRIPT_DIR/../Crews.PlanningCenter.Models.Generators/bin/Debug/$GENERATOR_FRAMEWORK/Crews.PlanningCenter.Models.Generators.dll" \
 	"$SCRIPT_DIR/PlanningCenterEntityGenerator.tt"
 
 printf "Generating parameters...\n"
@@ -30,7 +32,7 @@ t4 \
 	-r "$HOME/.nuget/packages/crews.extensions.http/$HTTP_EXTENSIONS_VERSION/lib/$HTTP_EXTENSIONS_FRAMEWORK/Crews.Extensions.Http.dll" \
 	-r "$HOME/.nuget/packages/crews.extensions.primitives/$PRIMITIVES_EXTENSIONS_VERSION/lib/$PRIMITIVES_EXTENSIONS_FRAMEWORK/Crews.Extensions.Primitives.dll" \
 	-r "$HOME/.nuget/packages/microsoft.extensions.options/$MS_OPTIONS_VERSION/lib/$MS_OPTIONS_FRAMEWORK/Microsoft.Extensions.Options.dll" \
-	-r "$SCRIPT_DIR/../Crews.PlanningCenter.Models.Generators/bin/Debug/net8.0/Crews.PlanningCenter.Models.Generators.dll" \
+	-r "$SCRIPT_DIR/../Crews.PlanningCenter.Models.Generators/bin/Debug/$GENERATOR_FRAMEWORK/Crews.PlanningCenter.Models.Generators.dll" \
 	"$SCRIPT_DIR/PlanningCenterParametersGenerator.tt"
 
 printf "\nCompleted in $SECONDS seconds.\n"
